43f720bca903eab0cc0d5100c7c4f13d8951ea54
mmj6452 <645277@naver.com>
2023-10-09 02:19:25 +0900

    Plot_update
    plot graph design update

--- NSY/ThreeSpaceAPI_py3_beta_V0.6/workspace/IMU_plot.py
+++ NSY/ThreeSpaceAPI_py3_beta_V0.6/workspace/IMU_plot.py
@@ -3,7 +3,7 @@
 duration을 조정하여 일정 시간 동안 데이터를 읽어오는 것도 가능 (단위: 초)
 interval을 조정하여 데이터를 읽어오는 속도를 조정할 수 있음
 실제로는 연산속도의 문제로 동일한 hz로 데이터를 읽어오는것은 불가능
-interval = 0.00385 실험적으로 얻은 200hz의 데이터를 읽어오는 속도
+interval = 0.00285 실험적으로 얻은 200hz의 데이터를 읽어오는 속도
 PC USB포트에 IMU를 연결하고 COM port를 지정해주면 실행
 Data format: "%float(OrientPitch),%float(OrientYaw),%float(OrientRoll),
               %float(CorrectedGyroX),%float(CorrectedGyroY),%float(CorrectedGyroZ),
@@ -23,6 +23,9 @@ com = USB_ExampleClass.UsbCom()
 sensor = ThreeSpaceSensor(com)
 
 # Initialize lists to store data
+OrientPitch = []
+OrientYaw = []
+OrientRoll = []
 acc_x = []
 acc_y = []
 acc_z = []
@@ -34,15 +37,18 @@ mag_y = []
 mag_z = []
 
 # Record data for 10 seconds with a 0.05-second interval
-duration = 10  # seconds
-interval = 0.00385  # seconds
+duration = 3  # seconds
+interval = 0.00285  # seconds
 end_time = time.time() + duration
 
 while time.time() < end_time:
     # Get reading from sensor, returns as tuple.
-    reading = sensor.getAllCorrectedComponentSensorData()
+    reading = sensor.getAllRawComponentSensorData()
 
     # Save component values to lists
+    OrientPitch.append(reading[0])
+    OrientYaw.append(reading[1])
+    OrientRoll.append(reading[2])
     acc_x.append(reading[3])
     acc_y.append(reading[4])
     acc_z.append(reading[5])
@@ -58,79 +64,59 @@ while time.time() < end_time:
 # Close the sensor
 sensor.cleanup()
 
-# Create a 3x3 grid of subplots
-fig, axs = plt.subplots(3, 3, figsize=(12, 8))
-fig.subplots_adjust(hspace=0.5, wspace=0.3)
+# Create a 2x2 grid of subplots
+fig, axs = plt.subplots(4, figsize=(15, 8))
+fig.subplots_adjust(hspace=0.5, wspace=0.2)
 
 acc_max = 12
 gyro_max = 2.5
 mag_max = 0.8
 
 
-# Plot Gyro X
-axs[0, 0].plot(acc_x)
-axs[0, 0].set_ylim(-acc_max, acc_max)
-axs[0, 0].set_xlabel('Sample')
-axs[0, 0].set_ylabel('Value')
-axs[0, 0].set_title('CorrectedGyroX')
-
+axs[0].set_xlabel('Sample')
+axs[0].set_ylabel('Value')
+axs[0].set_title('OrientPitch OrientYaw OrientRoll')
 # Plot Gyro Y
-axs[0, 1].plot(acc_y)
-axs[0, 1].set_ylim(-acc_max, acc_max)
-axs[0, 1].set_xlabel('Sample')
-axs[0, 1].set_ylabel('Value')
-axs[0, 1].set_title('CorrectedGyroY')
-
+axs[0].plot(OrientYaw,'g')  
 # Plot Gyro Z
-axs[0, 2].plot(acc_z)
-axs[0, 2].set_ylim(-acc_max, acc_max)
-axs[0, 2].set_xlabel('Sample')
-axs[0, 2].set_ylabel('Value')
-axs[0, 2].set_title('CorrectedGyroZ')
+axs[0].plot(OrientRoll,'b')
+
+
+# Plot Gyro X
+axs[1].plot(acc_x,'r')
+axs[1].set_ylim(-acc_max, acc_max)
+axs[1].set_xlabel('Sample')
+axs[1].set_ylabel('Value')
+axs[1].set_title('CorrectedGyroX CorrectedGyroY CorrectedGyroZ')
+# Plot Gyro Y
+axs[1].plot(acc_y,'g')
+# Plot Gyro Z
+axs[1].plot(acc_z,'b')
+
 
 # Plot Accel X
-axs[1, 0].plot(gyro_x)
-axs[1, 0].set_ylim(-gyro_max, gyro_max)
-axs[1, 0].set_xlabel('Sample')
-axs[1, 0].set_ylabel('Value')
-axs[1, 0].set_title('CorrectedAccelX')
-
+axs[2].plot(gyro_x,'r')
+axs[2].set_ylim(-gyro_max, gyro_max)
+axs[2].set_title('CorrectedAccelX CorrectedAccelY CorrectedAccelZ')
 # Plot Accel Y
-axs[1, 1].plot(gyro_y)
-axs[1, 1].set_ylim(-gyro_max, gyro_max)
-axs[1, 1].set_xlabel('Sample')
-axs[1, 1].set_ylabel('Value')
-axs[1, 1].set_title('CorrectedAccelY')
-
+axs[2].plot(gyro_y,'g')
 # Plot Accel Z
-axs[1, 2].plot(gyro_z)
-axs[1, 2].set_ylim(-gyro_max, gyro_max)
-axs[1, 2].set_xlabel('Sample')
-axs[1, 2].set_ylabel('Value')
-axs[1, 2].set_title('CorrectedAccelZ')
+axs[2].plot(gyro_z,'b')
+
+
 
 # Plot Mag X
-axs[2, 0].plot(mag_x)
-axs[2, 0].set_ylim(-mag_max, mag_max)
-axs[2, 0].set_xlabel('Sample')
-axs[2, 0].set_ylabel('Value')
-axs[2, 0].set_title('CorrectedMagX')
-
+axs[3].plot(mag_x,'r')
+axs[3].set_ylim(-mag_max, mag_max)
+axs[3].set_xlabel('Sample')
+axs[3].set_ylabel('Value')
+axs[3].set_title('CorrectedMagX CorrectedMagY CorrectedMagZ')
 # Plot Mag Y
-axs[2, 1].plot(mag_y)
-axs[2, 1].set_ylim(-mag_max, mag_max)
-axs[2, 1].set_xlabel('Sample')
-axs[2, 1].set_ylabel('Value')
-axs[2, 1].set_title('CorrectedMagY')
-
+axs[3].plot(mag_y,'g')
 # Plot Mag Z
-axs[2, 2].plot(mag_z)
-axs[2, 2].set_ylim(-mag_max, mag_max)
-axs[2, 2].set_xlabel('Sample')
-axs[2, 2].set_ylabel('Value')
-axs[2, 2].set_title('CorrectedMagZ')
+axs[3].plot(mag_z,'b')
+
 
 plt.tight_layout()
 plt.show()
-
 print(len(acc_x))
